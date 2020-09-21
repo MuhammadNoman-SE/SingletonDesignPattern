63d0d1e609a85c93c07f1045308c72398f74fa10
Muhammad Noman <Muhammad.Noman@bentley.com>
2020-09-21 09:07:57 +0500

    Singleton pattern in c#

---
+++ DesignPatternsInCSharp/Singleton/v1_Naive/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DesignPatternsInCSharp.Singleton.v1
+{
+    // Bad code
+#nullable enable
+    public sealed class Singleton
+    {
+        private static Singleton? _instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Console.WriteLine("Instance called.");
+
+                Logger.Log("Instance called.");
+                return _instance ??= new Singleton();
+            }
+        }
+
+        private Singleton()
+        {
+            Console.WriteLine("Constructor invoked.");
+
+            // cannot be created except within this class
+            Logger.Log("Constructor invoked.");
+        }
+    }
+}
